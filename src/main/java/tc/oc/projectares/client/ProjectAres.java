8cf716b9fb5e9d42ed0f74f08fb7c6c9b056ee3d
mrapple <tony@oc.tc>
2012-12-08 22:29:41 -0600

    Introducing, ModLoader

---
+++ src/main/java/tc/oc/projectares/client/ProjectAres.java
@@ -0,0 +1,12 @@
+package tc.oc.projectares.client;
+
+import net.minecraft.src.BaseMod;
+
+public final class ProjectAres extends BaseMod {
+
+    public void load() {}
+
+    public String getVersion() {
+        return "0.1";
+    }
+}
