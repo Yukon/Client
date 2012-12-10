548c576839d2342f1ee2de9f0466c41f5bb125ce
YukonAppleGeek <yukonvinecki@Yukon-Vineckis-MacBook-Pro.local>
2012-12-09 20:37:24 -0800

    Merge pull request #1 from YukonAppleGeek/master
    Fixed spelling error in method
    Added override to methods as required and more method hooks
    Added Project Ares server detection and gui hooks
    Made connect/disconnect start with on
    Made server domain a static final varable

--- src/main/java/net/minecraft/src/mod_ProjectAres.java
+++ src/main/java/net/minecraft/src/mod_ProjectAres.java
@@ -1,5 +1,6 @@
 package net.minecraft.src;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.src.*;
 
 import tc.oc.projectares.client.ProjectAres;
@@ -16,15 +17,45 @@ public final class mod_ProjectAres extends BaseMod {
         return null;
     }
 
+    @Override
     public void load() {
         this.pa = new ProjectAres();
     }
 
-    public void serverCustomPaylod(NetServerHandler serverHandler, Packet250CustomPayload packet) {
+    @Override
+    public void serverCustomPayload(NetServerHandler serverHandler, Packet250CustomPayload packet) {
         this.pa.onCustomPacket(serverHandler, packet.channel, packet.length, packet.data);
     }
 
+    @Override
     public String getVersion() {
         return "0.1";
+    }
+
+    @Override
+    public String getName() {
+        return "Project Ares Client";
+    }
+
+    @Override
+    public boolean onTickInGUI(float tick, Minecraft mc, GuiScreen screen) {
+        this.pa.onTickInGUI(tick, mc, screen);
+        return true;
+     }
+
+    @Override
+    public boolean onTickInGame(float tick, Minecraft mc) {
+        this.pa.onTickInGame(tick, mc);
+        return true;
+    }
+
+    @Override
+    public void clientConnect(NetClientHandler handler) {
+        this.pa.onConnect(handler);
+    }
+
+    @Override
+    public void clientDisconnect(NetClientHandler handler) {
+        this.pa.onDisconnect(handler);
     }
 }
--- src/main/java/tc/oc/projectares/client/ProjectAres.java
+++ src/main/java/tc/oc/projectares/client/ProjectAres.java
@@ -1,9 +1,17 @@
 package tc.oc.projectares.client;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.GuiScreen;
+import net.minecraft.src.ModLoader;
+import net.minecraft.src.NetClientHandler;
 import net.minecraft.src.NetServerHandler;
+import net.minecraft.src.mod_ProjectAres;
 
 public final class ProjectAres {
     private static ProjectAres pa;
+
+    public boolean onPA = false;
+    public static final String serverDomain = "oc.tc";
 
     public static ProjectAres get() {
         return pa;
@@ -11,7 +19,23 @@ public final class ProjectAres {
 
     public ProjectAres() {
         pa = this;
+        ModLoader.setInGUIHook(mod_ProjectAres.get(), true, false);
+        ModLoader.setInGameHook(mod_ProjectAres.get(), true, false);
     }
 
     public void onCustomPacket(NetServerHandler serverHandler, String channel, int length, byte[] data) {}
+
+    public void onTickInGUI(float tick, Minecraft mc, GuiScreen screen) {}
+
+    public void onTickInGame(float tick, Minecraft mc) {}
+
+    public void onConnect(NetClientHandler handler) {
+        if (handler.getNetManager().getSocketAddress().toString().contains(serverDomain)) {
+            onPA = true;
+        }
+    }
+
+    public void onDisconnect(NetClientHandler handler) {
+        if (onPA) onPA = false;
+    }
 }
